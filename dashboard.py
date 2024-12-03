b2c4af43b41bd0b57af997e19b956da4d05dab96
Timur Mamadaliyev <mamadaliyevtv@gmail.com>
2024-12-03 12:45:50 +0800

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -7,10 +7,6 @@ Original file is located at
     https://colab.research.google.com/drive/1i_HpBPRtNsnbNf9AVN2RCv2pXIDUIKDW
 """
 
-!pip install dash
-!pip install plotly
-!pip install dash-bootstrap-components
-
 import dash
 from dash import dcc, html, Input, Output
 import dash_bootstrap_components as dbc
